a303ec62e9d0acb62203e444505a3c84e9e34c0b
Amaka Onwuli <amonwuli@gmail.com>
2025-04-13 12:14:44 +0100

    updated css properties

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Weather">
       <h1>Weather App</h1>
       <Weather />
       </header>
